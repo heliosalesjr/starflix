75d9c4bb3b050b50096202a4281e6fa19370445d
heliosalesjr <heliosalesjr@gmail.com>
2023-08-31 13:42:06 -0300

    first setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Inicio from '../src/pages/Inicio'
+
+// repositorio inicial: https://github.com/alura-cursos/cinetag/tree/arquivos-iniciais
+
+function App() {
+  return (
+    <>
+      <Inicio />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Inicio/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Inicio.module.css'
+
+function Inicio() {
+  return (
+    <>
+        <h1>Olah monah!</h1>
+        <button>Me clickah!</button>
+    </>
+    
+  )
+}
+
+export default Inicio
